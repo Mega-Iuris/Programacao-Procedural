a82237b6c1262e3fb68875517b76d0e5559f8916
Mega-Iuris <iuris.p.medeiros@outlook.com>
2024-03-03 12:09:09 -0300

    Aula7

---
+++ Listas/Aula7/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+class Program 
+{
+    static void Main(String[]args)
+    {
+        /*int somatorio = 0;
+        int valorInserido=0;
+
+        do
+        {          
+            Console.WriteLine("Digite o valor que você desja inserir: ");
+             valorInserido =Convert.ToInt32(Console.ReadLine());
+
+            if(valorInserido % 2 ==1)
+            {
+                somatorio = somatorio + valorInserido;
+            }
+        }
+        while(valorInserido <=50);
+        Console.WriteLine($"A soma dos nome impares eh: {somatorio}");*/
+
+       /*const string RESPOSTA_CORRETA = "karpov";
+        const int PONTUACAO_INICIAL = 100;
+        const int NUMERO_MAXIMO_TENTATIVAS = 3;
+        
+        int contadorTentativas = 1;
+
+        do
+        {
+            System.Console.WriteLine("\n[Dica] - foi campeão mundial de xadrez");
+            System.Console.WriteLine("Digite o valor do campo");
+            string respostaUsuario = Console.ReadLine();
+
+            if(respostaUsuario == RESPOSTA_CORRETA)
+            {
+                double pontuacaofinal = PONTUACAO_INICIAL / contadorTentativas;
+                System.Console.WriteLine("Você Acertou!!! A sua resposa foi: " + pontuacaofinal);
+                break;
+            }
+            contadorTentativas++;
+        }
+        while(contadorTentativas <= NUMERO_MAXIMO_TENTATIVAS);*/
+
+        /*System.Console.WriteLine("Digite o valor inteiro para exibir a tabuada: ");
+        int valor = Convert.ToInt32(Console.ReadLine());
+
+        for(int i =0; i <=10; i++)
+        {
+            System.Console.WriteLine($" {valor} X {i} = {valor * i}");
+        }*/
+
+        
+
+
+    }
+}
