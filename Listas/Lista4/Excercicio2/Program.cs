f06d08068d616cc2556ca46bf7215ab979a29349
Mega-Iuris <iuris.p.medeiros@outlook.com>
2024-03-14 16:26:05 -0300

    Lista4 - Laços de repetição aninhados

---
+++ Listas/Lista4/Excercicio2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string []args)
+    {
+        int altura = 0;
+
+        do
+        {
+            Console.Write($"Digite um valor entre (1 e 9): ");
+            altura = Convert.ToInt32(Console.ReadLine());
+        }
+        while(altura < 1 || altura > 9);
+
+        for (int i = 1; i <= altura; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j);
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
---
+++ Listas/Lista4/Excercicio3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string []args)
+    {
+        const int COLUNAS = 7;
+        const int LINHAS = 7;
+
+        Tabuleiro.InicializaTabuleiro();
+
+        for (int i = 0; i <= COLUNAS; i++)
+        {
+            for (int j = 0; j <= LINHAS; j++)
+            {
+                if (i == 0 || i == LINHAS || j == 0 || j == COLUNAS)
+                {
+                    Tabuleiro.Adicionar(i, j, "S");
+                }
+            }
+        }
+
+        Tabuleiro.ImprimeTabuleiro();
+    }
+}
